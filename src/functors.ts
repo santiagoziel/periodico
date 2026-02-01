b9de64df75e5c518cf89810a0c612aea5dd9a264
Santiago Torres <45303560+santiagoziel@users.noreply.github.com>
2026-02-01 13:46:16 -0600

    Source interface definition (#1)
    * adding categorical deifnitions of concepts
    
    * monadic operations
    
    * adding error models
    
    * news source interface definition

---
+++ src/functors.ts
@@ -0,0 +1,53 @@
+import { GeneralError } from "./error-models"
+
+export type Success<A extends object> = {is: "Successful" } & A
+export type Failure<B extends object> = {is: "Failed" } & B
+export type Attempt<A extends object,B extends object> = Success<A> | Failure<B>
+export type AttemptToFetch<A extends object> = Promise<Attempt<A, GeneralError>>
+
+export type Unit = {}
+
+export const success = <A extends object>(a: A): Success<A> => ({is: "Successful", ...a})
+export const failure = <B extends object>(b: B): Failure<B> => ({is: "Failed", ...b})
+
+export const failedThe = <A extends object, B extends object>(v: Attempt<A, B>): v is Failure<B> => v.is === "Failed";
+export const succeededInThe = <A extends object, B extends object>(v: Attempt<A, B>): v is Success<A> => v.is === "Successful";
+
+export const payloadFromTheFailed = <B extends object>(v: Failure<B>): B => {
+    const { is, ...failure } = v;
+    return failure as B;
+};
+
+export const payloadFromTheSuccessful = <A extends object>(v: Success<A>): A => {
+    const { is, ...success } = v;
+    return success as A;
+};
+
+//Kleisli categorical composition
+export const tryTo = <A extends object, B extends object, E extends object, R extends Attempt<B, E> | Promise<Attempt<B, E>>>
+(f: (x: A) => R, attempt: Attempt<A, E>): R => 
+    (failedThe(attempt) ? attempt : f(payloadFromTheSuccessful(attempt))) as R;
+
+export const lifted = <A extends object, B extends object>
+(f: (x: A) => B): (a: A) => Success<B> =>
+  (a: A): Success<B> => success(f(a));
+
+export const tryToRun = <A extends object, B extends object, E extends object, R extends Attempt<B, E> | Promise<Attempt<B, E>>>
+(f: (x: A) => B, attempt: Attempt<A, E>): R => 
+    tryTo(lifted(f), attempt) as R;
+
+export const reframeTheFailed = <A extends object, E extends object, F extends object>
+(attempt: Attempt<A, E>, f: (e: E) => F): Attempt<A, F> =>
+    succeededInThe(attempt) ? attempt : failure(f(payloadFromTheFailed(attempt)));
+
+export const resolveThe = <A extends object, E extends object, R>
+(attempt: Attempt<A, E>, onSuccess: (a: A) => R, onFailure: (e: E) => R): R =>
+    succeededInThe(attempt) 
+        ? onSuccess(payloadFromTheSuccessful(attempt)) 
+        : onFailure(payloadFromTheFailed(attempt));
+
+export const buildSuccessPayloadFrom = <A extends object, E extends object>
+(attempt: Attempt<A, E>, fromFailure: (e: E) => A): A =>
+    succeededInThe(attempt) 
+        ? payloadFromTheSuccessful(attempt) 
+        : fromFailure(payloadFromTheFailed(attempt));
--- src/sources/source-interface.ts
+++ src/sources/source-interface.ts
@@ -1,2 +1,9 @@
-
-
+import { SourceName } from "../constants";
+import { ArticleIdentifier, ArticleTitle, RawArticlePayload } from "../entities";
+import { AttemptToFetch } from "../functors";
+        
+export interface NewsSource {
+    name: SourceName;
+    getTitles(): AttemptToFetch<{titles: ArticleTitle[]}>
+    fetchArticle(articleInfo: ArticleIdentifier): AttemptToFetch<RawArticlePayload>
+}
