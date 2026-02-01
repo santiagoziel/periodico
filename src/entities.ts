d1fe5a0d02d7f5ea87995cce6c0fb06a1a891df6
Santiago Torres <santiago_oziel92@hotmail.com>
2026-01-31 18:14:34 -0600

    Initial enitity definitions

---
+++ src/entities.ts
@@ -0,0 +1,56 @@
+export type SourceName = "intoleranciadiario" | "sintesis" | "pueblaonline";
+
+export type ArticleTitle = {
+    url: string,
+    title: string
+}
+
+export type ArticlesInfo = Record<SourceName, ArticleTitle>
+
+export type UniqueTitle = {
+    source: SourceName,
+    title: string
+}
+
+export type TitleGroup = UniqueTitle[]
+
+export type EmbeddedArticleTitles = {
+    union: TitleGroup[],
+    single: TitleGroup
+}
+
+export type ArticleIdentifier = {
+    source: SourceName;
+    title: string;
+    url: string;
+  };
+
+export type RawArticlePayload = {
+    facts: string,
+    url: string, //just to attach at the end, the call has been made already
+    relevantPersons?: string[]
+}
+
+export type UnionArticlePayload = RawArticlePayload[]
+
+export type ProcessSingleArticleInput = {
+    type: "single",
+    facts: string,
+    url: string,
+    relevantPersons?: string[]
+}
+
+export type ProcessUnionArticleInput = {
+    type: "union",
+    facts: string[],
+    urls: string[],
+    relevantPersons?: string[]
+}
+
+export type ProcessArticleInput = ProcessSingleArticleInput | ProcessUnionArticleInput
+
+export type ProcessedArticle = {
+    processedTile: string,
+    text: string,
+    personName?: string
+}
---
+++ src/main.ts
@@ -0,0 +1,1 @@
+//obtener una lista de links y titulos de cada fuente
