9f6cc704f53e99b7547f8a9e13f546da02b4134a
Santiago Torres <santiago_oziel92@hotmail.com>
2026-02-01 13:01:26 -0600

    adding categorical deifnitions of concepts

---
+++ src/functors.ts
@@ -0,0 +1,35 @@
+export type Success<A extends object> = {is: "Successful" } & A
+export type Failure<B extends object> = {is: "Failed" } & B
+export type Attempt<A extends object,B extends object> = Success<A> | Failure<B>
+export type PromiseToAttempt<A extends object,B extends object> = Promise<Attempt<A, B>>
+
+export type Unit = {}
+
+export const success = <A extends object>(a: A): Success<A> => ({is: "Successful", ...a})
+export const failure = <B extends object>(b: B): Failure<B> => ({is: "Failed", ...b})
+
+export const failedThe = <A extends object, B extends object>(v: Attempt<A, B>): v is Failure<B> => v.is === "Failed";
+export const succeededInThe = <A extends object, B extends object>(v: Attempt<A, B>): v is Success<A> => v.is === "Successful";
+
+export const payloadFromTheFailed = <B extends object>(v: Failure<B>): B => {
+    const { is, ...failure } = v;
+    return failure as B;
+};
+
+export const payloadFromTheSuccessful = <A extends object>(v: Success<A>): A => {
+    const { is, ...success } = v;
+    return success as A;
+};
+
+//Kleisli categorical composition
+export const tryTo = <A extends object, B extends object, E extends object, R extends Attempt<B, E> | PromiseToAttempt<B, E>>
+(f: (x: A) => R, attempt: Attempt<A, E>): R => 
+    (failedThe(attempt) ? attempt : f(payloadFromTheSuccessful(attempt))) as R;
+
+export const lifted = <A extends object, B extends object>
+(f: (x: A) => B): (a: A) => Success<B> =>
+  (a: A): Success<B> => success(f(a));
+
+export const tryToRun = <A extends object, B extends object, E extends object, R extends Attempt<B, E> | PromiseToAttempt<B, E>>
+(f: (x: A) => B, attempt: Attempt<A, E>): R => 
+    tryTo(lifted(f), attempt) as R;
