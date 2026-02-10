aad19058a5eddf1516ff3faf50ca3e5e6bad1139
Santiago Torres <santiago_oziel92@hotmail.com>
2026-02-09 21:24:39 -0600

    preparing fetched files for upload

--- src/sources/dummy/dummy-source2.ts
+++ src/sources/dummy/dummy-source2.ts
@@ -7,14 +7,14 @@ export const dummySource2: NewsSource = {
     name: "dummy2",
     getTitles: async () => {
         return success({titles: [
-            {url: "https://example.com/news/5", title: "Fuerte sismo de 6.5 grados afecta región costera de Oaxaca"},
-            {url: "https://example.com/news/6", title: "Lluvias intensas provocan inundaciones en Tabasco"},
-            {url: "https://example.com/news/7", title: "México derrota a USA con marcador de 3 a 1 en encuentro de fútbol"},
-            {url: "https://example.com/news/8", title: "Científicos mexicanos descubren nueva especie de mariposa"},
+            {url: "https://dummy2.com/news/5", title: "Fuerte sismo de 6.5 grados afecta región costera de Oaxaca"},
+            {url: "https://dummy2.com/news/6", title: "Lluvias intensas provocan inundaciones en Tabasco"},
+            {url: "https://dummy2.com/news/7", title: "México derrota a USA con marcador de 3 a 1 en encuentro de fútbol"},
+            {url: "https://dummy2.com/news/8", title: "Científicos mexicanos descubren nueva especie de mariposa"},
         ]})
     },
 
     fetchArticle: async (articleInfo: ArticleIdentifier) => {
-        return success({facts: "Contenido del artículo de noticias", url: articleInfo.url})
+        return success({facts: `Contenido del artículo ${articleInfo.title} for source ${articleInfo.source}`, url: articleInfo.url})
     }
 }
