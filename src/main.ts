d4ab87fe1c7227f34f856f47208c61b2f3605d27
Santiago Torres <santiago_oziel92@hotmail.com>
2026-02-11 20:26:47 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { Agent } from "./agent/agent"
 import { Application } from "./app/application"
 import { DSL } from "./app/dsl"
+import { NewsEditor } from "./newsEditor/news-editor"
 import { dummySource } from "./sources/dummy/dummy-source"
 import { dummySource2 } from "./sources/dummy/dummy-source2"
 import { dummySource3 } from "./sources/dummy/dummy-source3"
 
 const main = async () => {
-    const application = new Application([dummySource, dummySource2, dummySource3], new DSL(), new Agent())
+    const agent = new Agent()
+    const newsEditor = new NewsEditor(agent)
+    const application = new Application([dummySource, dummySource2, dummySource3], new DSL(), agent, newsEditor)
     const articlesInfo = await application.run()
     console.log(JSON.stringify(articlesInfo, null, 4))
 }
