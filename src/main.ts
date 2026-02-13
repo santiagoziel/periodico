1765cdea98a1244c86d224a1c82da13fe1dcd4d4
Santiago Torres <santiago_oziel92@hotmail.com>
2026-02-13 17:13:31 -0600

    adding real source

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Application } from "./app/application"
 import { DSL } from "./app/dsl"
 import { NewsEditor } from "./newsEditor/news-editor"
 import { Publisher } from "./publisher/publisher"
+import { DiarioSource } from "./sources/diario.source"
 import { dummySource } from "./sources/dummy/dummy-source"
 import { dummySource2 } from "./sources/dummy/dummy-source2"
 import { dummySource3 } from "./sources/dummy/dummy-source3"
@@ -17,7 +18,9 @@ const main = async () => {
 
     const publisher = new Publisher([localFiles, cloudFiles])
 
-    const application = new Application([dummySource, dummySource2], new DSL(), agent, newsEditor, publisher)
+    const intoleranciaSource = new DiarioSource()
+
+    const application = new Application([intoleranciaSource], new DSL(), agent, newsEditor, publisher)
     console.log("--------------------Start------------------------------------------")
     const articlesInfo = await application.run()
     console.log(JSON.stringify(articlesInfo, null, 4))
